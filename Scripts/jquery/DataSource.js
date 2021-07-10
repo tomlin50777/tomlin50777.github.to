7329d3ef6db210b4167b09c510a76823fc928bb7
S.Y.Lin <tomlin50777@hotmail.com>
2021-07-10 13:03:19 +0800

    Jquery load Test

---
+++ Scripts/jquery/DataSource.js
@@ -0,0 +1,35 @@
+var menuTabPanelItem = [
+    { text: "user" },
+    { text: "analytics" },
+    { text: "customers" },
+    { text: "search" },
+    { text: "favorites" },
+    { text: "additional" },
+    { text: "clients" },
+    { text: "orders" },
+    { text: "shipment" }
+];
+var noteItem = [
+    { text: "user" },
+    { text: "analytics" },
+    { text: "customers" },
+    { text: "search" },
+    { text: "favorites" },
+    { text: "additional" },
+    { text: "clients" },
+    { text: "orders" },
+    { text: "shipment" }
+];
+var kanBanItem = [
+    { text: "Data" },
+    { text: "Box" }
+];
+var musicItem = [
+    { text: "Show" },
+    { text: "SY" }
+];
+
+var menuTabPanelItem = [
+    { ID: 1, title: 'Note', item: kanBanItem },
+    { ID: 2, title: 'KanBan', item: noteItem },
+];
---
+++ Scripts/jquery/Layout.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('#menuTab').dxTabs({
+        dataSource: menuTabItem
+    });
+
+
+    var tabPanel = $("#menuTabPanel").dxTabPanel({
+        dataSource: employees.slice(0, 3),
+        itemTitleTemplate: titleTemplate,
+        itemTemplate: itemTemplate,
+        height: 410,
+        deferRendering: false,
+        showNavButtons: true,
+        repaintChangesOnly: true
+    }).dxTabPanel("instance");
+    $('@#ontent').load('View/KanBan/data.html');
+});
