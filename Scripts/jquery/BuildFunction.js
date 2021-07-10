aa8b4fc447e786ca462f632f62f2b839bdfc8e68
S.Y.Lin <tomlin50777@hotmail.com>
2021-07-10 17:22:40 +0800

    KanBanTest

--- Scripts/jquery/BuildFunction.js
+++ Scripts/jquery/BuildFunction.js
@@ -10,3 +10,98 @@ function BuildKanBanData() {
         showScrollbar: "onScroll",
     })
 }
+
+
+function BuildKanBanBox() {
+    $("#RowButton").dxButton({
+        stylingMode: "outlined",
+        text: "Outlined",
+        type: "success",
+        width: 120,
+        onClick: function (e) {
+            Stage_Final(kanbandata);
+            //switch (e.component.option('stage')) {
+            //    case 0:
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //}
+        }
+    });
+    $("#ColButton").dxButton({
+        stylingMode: "outlined",
+        text: "Outlined",
+        type: "£¡",
+        width: 120,
+        onClick: function (e) {
+            //switch (e.component.option('stage')) {
+            //    case 0:
+            //    case 1:
+            //    case 2:
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //}
+        }
+    });
+}
+function Stage_Final(data) {
+    var system = Object.keys(data);
+    $('#BoxBaseDiv').append('<div id="Popup_Base_Col"></div>');
+    for (var countZ = 0; countZ < system.length; countZ++) {
+        var subsystem = Object.keys(data[system[ccountZ]]);
+        $('#Popup_Base_Col').append('<div id="Popup_System_Block_' + countZ + '"></div>');
+        $('#Popup_System_Block_' + countZ).append('<div id="Popup_System_Name_' + countZ + '" data-options="dxItem: {ratio: 1}">' + system[countZ] + '</div>');
+        $('#Popup_System_Block_' + countZ).append('<div id="Popup_System_Content_' + countZ + '" data-options="dxItem: {ratio: 9}"></div>');
+        for (var countY = 0; countY < subsystem.length; countY++) {
+            var detail = Object.keys(data[system[ccountZ]][subsystem[countY]]);
+            $('#Popup_System_Content_' + countZ).append('<div id="Popup_Subsystem_Block_' + countZ + '_' + countY + '"></div>');
+            $('#Popup_Subsystem_Block_' + countZ + '_' + countY).append('<div id="Popup_Subsystem_Name_' + countZ + '_' + countY + '" data-options="dxItem: {ratio: 1}">' + subsystem[countY] + '</div>');
+            $('#Popup_Subsystem_Block_' + countZ + '_' + countY).append('<div id="Popup_Subsystem_Content_' + countZ + '_' + countY + '" data-options="dxItem: {ratio: 9}"></div>');
+            $('#Popup_Subsystem_Content_' + countZ + '_' + countY).append('<div id="Popup_DetailBlock_' + countZ + '_' + countY + '"></div>');
+            for (var countX = 0; countX < detail.length; countX++) {
+                $('#Popup_DetailBlock_' + countZ + '_' + countY).append('<div id="Popup_Detail_Content_' + countZ + '_' + countY + '_' + countX + '" data-options="dxItem: {ratio: 1}"></div>');
+                $('#Popup_Detail_Content_' + countZ + '_' + countY + '_' + countX).append('<div id="Popup_Detail_Name_' + countZ + '_' + countY + '_' + countX + '" >' + detail[countX] + '</div>');
+                $('#Popup_DetailBlock_' + countZ + '_' + countY + '_' + countX).append('<div id="Popup_Detail_Value_' + countZ + '_' + countY + '_' + countX + '" >' + data[system[countZ]][subsystem[countY]][detail[countX]] + '</div>');
+            }
+        }
+    }
+    for (var countZ = 0; countZ < system.length; countZ++) {
+        var subsystem = Object.keys(data[system[ccountZ]]);
+        $("#Popup_System_Block_" + countZ).dxBox({
+            direction: "row",
+            width: "100%"
+        });
+        for (var countY = 0; countY < subsystem.length; countY++) {
+            var detail = Object.keys(data[system[ccountZ]][subsystem[countY]]);
+            $("#Popup_Subsystem_Block_" + countZ + '_' + countY).dxBox({
+                direction: "row",
+                width: "100%"
+            });
+            $("#Popup_DetailBlock_" + countZ + '_' + countY).dxBox({
+                direction: "row",
+                width: "100%"
+            });
+        }
+    }
+
+}
+function Stage_0_Col(data) {
+    var system = Object.keys(data);
+    $('#BoxBaseDiv').append('<div id="Popup_Base_Col"></div>');
+    for (var count = 0; count < system.length; count++) {
+        $('#Popup_Base_Col').append('<div id="Popup_System_Block_' + count + '"></div>');
+    }
+}
+function Stage_0_Row(data) {
+    var system = Object.keys(data);
+    $('#BoxBaseDiv').append('<div id="Popup_Base_Row"></div>');
+    $('#Popup_Base_Row').append('<div id="Popup_System_Name_Block" data-options="dxItem: {ratio: 1}"></div>');
+    $('#Popup_Base_Row').append('<div id="Popup_Subsystem_Block" data-options="dxItem: {ratio: 4}"></div>');
+
+    for (var count = 0; count < system.length; count++) {
+        $('#Popup_Base_Col').append('<div id="Popup_System_Block_' + count + '">');
+    }
+}
--- Scripts/jquery/DataSource.js
+++ Scripts/jquery/DataSource.js
@@ -21,8 +21,8 @@ var noteItem = [
     { text: "shipment" }
 ];
 var kanBanItem = [
-    { text: 'Sample Data', url: 'View/KanBan/data.html', bulidFunction: BuildKanBanData },
-    { text: 'Box' }
+    { text: 'Sample Data', url: 'View/KanBan/Data.html', bulidFunction: BuildKanBanData },
+    { text: 'Box', url: 'View/KanBan/Box.html', bulidFunction:BuildKanBanBox}
 ];
 var musicItem = [
     { text: "Show" },
@@ -33,3 +33,121 @@ var menuTabPanelItem = [
     { ID: 1, title: 'Note', item: noteItem },
     { ID: 2, title: 'KanBan', item: kanBanItem },
 ];
+
+var kanbandata =
+{
+    "Power": {
+        "Power": {
+            "Tai-Power": 0.0,
+            "Emergency": 0.0,
+            "UPS": 0.0,
+            "Sub-station": 0.0
+        }
+    },
+    "JiXei": {
+        "AMC": {
+            "TS": 0.0,
+            "HCL": 0.0,
+            "NH3": 0.0,
+            "TOC": 0.0
+        },
+        "CR": {
+            "Temperature": 0.0,
+            "Humidity": 0.0
+        },
+        "EXHAUST": {
+            "E-BGR": 0.0,
+            "E-SEX": 0.0
+        },
+        "PV": {
+            "BE-PV": 0.0
+        }
+    },
+    "PCW": {
+        "LOOP3": {
+            "TEMP": 0.0,
+            "Pressure": 0.0
+        },
+        "LOOP5": {
+            "TEMP": 0.0,
+            "Pressure": 0.0
+        }
+    },
+    "UPW": {
+        "LOOP4": {
+            "Particle": 0.0,
+            "Resistivity": 0.0,
+            "SiO2": 0.0,
+            "TOC": 0.0,
+            "DO": 0.0,
+            "TEMP": 0.0,
+            "Pressure": 0.0
+        }
+    },
+    "Bulk gas": {
+        "4 %HZ/N2": {
+            "AGCM100": 0.0,
+            "AGCM101": 0.0,
+            "BGCM301": 0.0
+        }
+    },
+    "BGS": {
+        "PAR": {
+            "PMS": 0.0,
+            "H2O": 0.0,
+            "O2": 0.0,
+            "CO": 0.0,
+            "CO2": 0.0,
+            "H2": 0.0,
+            "THC": 0.0,
+            "Pressure": 0.0,
+            "N2": 0.0
+        },
+        "PHE": {
+            "PMS": 0.0,
+            "H2O": 0.0,
+            "O2": 0.0,
+            "CO": 0.0,
+            "CO2": 0.0,
+            "H2": 0.0,
+            "THC": 0.0,
+            "Pressure": 0.0,
+            "N2": 0.0
+        },
+        "PN2": {
+            "PMS": 0.0,
+            "H2O": 0.0,
+            "O2": 0.0,
+            "CO": 0.0,
+            "CO2": 0.0,
+            "H2": 0.0,
+            "THC": 0.0,
+            "Pressure": 0.0,
+            "N2": 0.0
+        },
+        "PO2": {
+            "PMS": 0.0,
+            "H2O": 0.0,
+            "AR": 0.0,
+            "CO": 0.0,
+            "CO2": 0.0,
+            "H2": 0.0,
+            "THC": 0.0,
+            "Pressure": 0.0,
+            "N2": 0.0
+        },
+        "XCDA": {
+            "PMS": 0.0,
+            "H2O": 0.0,
+            "NH3": 0.0,
+            "TS": 0.0,
+            "TOC": 0.0
+        },
+        "CDA": {
+            "PT": 0.0
+        },
+        "GNZ": {
+            "PT": 0.0
+        }
+    }
+};
